a63b1a38c776e3ea562667d39ca36c0795064e3a
Gary Crosby <gary.crosby@usask.ca>
2025-10-10 20:47:29 -0600

    Remove test for planet.add_perm_moon CHange planet.mass_kg to mass_gigatons

--- unit_tests.py
+++ unit_tests.py
@@ -59,7 +59,7 @@ class TestPlanet(unittest.TestCase):
 
         # Venus
         assert self.venus.name == "Venus", "Planet name incorrect"
-        assert self.venus.mass_kg == 4.87e+24, "Planet mass incorrect"
+        assert self.venus.mass_gigatons == 4870000000000, "Planet mass incorrect"
         assert self.venus.type == "Terrestrial", "Planet type incorrect"
         assert self.venus.orbit_km == 108200000, "Planet distance from Sun incorrect"    
         assert self.venus.orbit_yr == 0.62, "Planet orbital period incorrect"
@@ -68,7 +68,7 @@ class TestPlanet(unittest.TestCase):
         
         # Earth
         assert self.earth.name == "Earth", "Planet name incorrect"
-        assert self.earth.mass_kg == 5.97e+24, "Planet mass incorrect"
+        assert self.earth.mass_gigatons == 5970000000000, "Planet mass incorrect"
         assert self.earth.type == "Terrestrial", "Planet type incorrect"
         assert self.earth.orbit_km == 149600000, "Planet distance from Sun incorrect"    
         assert self.earth.orbit_yr == 1.00, "Planet orbital period incorrect"
@@ -77,20 +77,12 @@ class TestPlanet(unittest.TestCase):
 
         # Jupiter
         assert self.jupiter.name == "Jupiter", "Planet name incorrect"
-        assert self.jupiter.mass_kg == 1.90e+27, "Planet mass incorrect"
+        assert self.jupiter.mass_gigatons == 1900000000000000, "Planet mass incorrect"
         assert self.jupiter.type == "Gas Giant", "Planet type incorrect"
         assert self.jupiter.orbit_km == 778500000, "Planet distance from Sun incorrect"    
         assert self.jupiter.orbit_yr == 11.86, "Planet orbital period incorrect"
         assert self.jupiter.moons_perm == ["Io", "Europa", "Ganymede", "Callisto"], "Planet permanent moons count incorrect"  
         assert self.jupiter.moons_prov_n == 54, "Planet provisional moons count incorrect"
-
-    def test_add_moon_perm(self):
-        """Test adding a permanent moon to a planet."""
-        
-        # Add a new moon to Jupiter
-        self.jupiter.add_moon_perm("Thebe")
-        assert self.jupiter.moons_perm == ["Io", "Europa", "Ganymede", "Callisto", "Thebe"], "Failed to add new permanent moon to Venus"
-        assert len(self.jupiter.moons_perm) == 5, "Permanent moons count incorrect after adding new moon"
 
     # Add test(s) for DataSources here
 
